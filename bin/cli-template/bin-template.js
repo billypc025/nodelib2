f27b2e6220f46612da069c9ace04c4cac8e060ee
billpc025 <billpc@163.com>
2017-09-12 17:03:26 +0800

    update

--- bin/cli-template/bin-template.js
+++ bin/cli-template/bin-template.js
@@ -3,25 +3,38 @@
  */
 var g = require("nodeLib");
 
+
+
 //调用本工程类库
 //var timeTool = require("../utils/TimeTool");
 
 //调用nodelib工具类库
 //var zip = require("nodeLib/utils/zip");
 
-var arg = getArgs(); //获取传入的参数
 
-init();
+//方式一：带命令及参数
+/*
+var globalCmd = require("../utils/actionList")();
 
-function init()
+(function ()
 {
-	//初始化
-	//...
-	start();
-}
+	globalCmd.add(init); //初始化init命令
+})()
 
-function start()
+!globalCmd.exe.apply(globalCmd.exe, getArgs()) && process.exit();
+
+//init命令的具体实现
+function init(...arg)
 {
-	//开始运行
 	//...
 }
+*/
+
+
+
+//方式二：仅参数
+/*
+ var arg=getArgs();
+ var msg=getArgs(0,"hello world");
+ trace(msg);
+ */
