cdbea03cbb888c14b3076c0d8b57e4f8186e1ae0
billpc025 <billpc@163.com>
2017-09-13 15:50:57 +0800

    update

--- bin/cli.js
+++ bin/cli.js
@@ -413,8 +413,6 @@ function addServer($routerName, $serverType)
 		var testFilePath = getCliPath("./bin/cli-template/test.js");
 		if (!projExist("./module/test.js"))
 		{
-			trace(123123)
-			trace(123123)
 			copyFile("./bin/cli-template/test.js", "./module/test.js");
 			trace("Create File:" + getProjPath("./module/test.js"));
 		}
