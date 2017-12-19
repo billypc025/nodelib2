0124a9c77099c8174db68a2fb4141775f5986a27
billpc025 <billpc@163.com>
2017-12-19 20:34:36 +0800

    update

--- bin/cli-template/test-template/test.js
+++ bin/cli-template/test-template/test.js
@@ -1,12 +1,22 @@
 //用于加入一条提示信息
-addMsg("提示信息")
+addMsg("------------提示信息-------------");
 
 //用于加入一个接口测试
 addTest("user/login", {
 	account: "18658110520",
-	$pass: ["11223344", "1235234", ""]
-}, function (d)
+	$pass: ["11223344", "1235234", "", null, "  ", "000", "*"]
+}, function (status, d, param, success, error)
 {
-	trace(d)
-	this.end(); //用例结束必须调用这个方法
+	if (status)
+	{
+		mysql.query("select id,account,pass,tel,name,rights from user	where account='18658110520'", (d)=>
+		{
+			this.end({}, {id: d[0].id});
+		});
+	}
+	else
+	{
+		this.end({pass: "1235234"}, {code: "*"});
+		this.end({pass: "*"}, {code: "*"});
+	}
 });
