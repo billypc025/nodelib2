901e046db645951ae0bf97ae8973ea305fcda503
billypc <billypc@163.com>
2019-01-29 17:37:19 +0800

    update

--- bin/openthis.js
+++ bin/openthis.js
@@ -4,8 +4,9 @@
  */
 var open = require('opener');
 var localHost = require("../utils/localhost");
-var serve = require("serve");
 require("../global");
+const handler = require('serve-handler');
+const http = require('http');
 
 var localIp = localHost.getLocalIp();
 var serverPort = getArgs(0, null) || 50000;
@@ -16,12 +17,23 @@ function testPort()
 {
 	localHost.portIsFree(serverPort, function ()
 	{
-		serve("", {
-			port: serverPort,
-			ignore: ["node_modules/", ".gitignore", ".idea/", "src/"]
+
+		const server = http.createServer((request, response) =>
+		{
+			return handler(request, response, {
+				"unlisted": [
+					"/node_modules",
+					".gitignore",
+					".idea",
+					"/src"
+				]
+			});
 		})
 
-		open("http://" + localIp + ":" + serverPort);
+		server.listen(serverPort, () =>
+		{
+			open("http://" + localIp + ":" + serverPort);
+		});
 	}, function ()
 	{
 		serverPort++;
